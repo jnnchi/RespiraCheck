710cd7ce0e50c2d1984bfa8ff4c79591d973180f
Jennifer Chiou <jennifer.r.chiou@gmail.com>
2025-01-08 16:12:43 -0500

    fixed error in target df type

--- data/data_loader.py
+++ data/data_loader.py
@@ -30,21 +30,23 @@ transform = transforms.Compose([
 
 dataset = pd.read_csv('data/spectrograms/numerical_labels.csv')
 
-# Split Data
+#numerical_data = dataset.drop("filepath", axis=1)
 
-numerical_data = dataset.drop("filepath", axis=1)
-labels = dataset["diagnosis"]
-features = dataset.drop("diagnosis", axis=1)
+# Split data first into training and testing so that we preserve the target variable (diagnosis) in both
+training_set, testing_set = train_test_split(dataset, test_size=0.15, shuffle=True)
 
-training_features, testing_features, training_target, testing_target = train_test_split(features, labels.to_frame(), test_size=0.15, shuffle=False)
+# filepaths
+training_paths = training_set["filepath"]
+testing_paths = testing_set["filepath"]
 
-training_paths = training_features["filepath"]
-training_features.drop("filepath")
+# Numerical feature datasets
+training_features = training_set.drop(["filepath", "diagnosis"], axis=1)
+testing_features = testing_set.drop(["filepath", "diagnosis"], axis=1)
 
-testing_paths = testing_features["filepath"]
-testing_features.drop("filepath")
+# Numerical target datasets - specify that they're dataframes otherwise it will be a Series
+training_target = pd.DataFrame(training_set["diagnosis"])
+testing_target = pd.DataFrame(testing_set["diagnosis"])
 
-print(training_features)
 # Make a np array of training images
 
 img_list = []
@@ -52,27 +54,22 @@ for path in training_paths:
     img = Image.open(path)
     img_array = np.array(img)
     img_list.append(img_array)
-    
+
 image_array = np.array(img_list)
 
 # Normalize Data
 
 scaler = MinMaxScaler()
-
 n_training_features = pd.DataFrame(scaler.fit_transform(training_features))
 n_testing_features = pd.DataFrame(scaler.fit_transform(testing_features))
 n_training_target = pd.DataFrame(scaler.fit_transform(training_target))
 n_testing_target = pd.DataFrame(scaler.fit_transform(testing_target))
 
 # Creating Tensors
-
 t_train_features = torch.Tensor(n_training_features.to_numpy())
 t_train_targets = torch.Tensor(n_training_target.to_numpy())
 t_train_images = torch.Tensor(image_array)
 
+# Training dataloader
 combined_tensor = TensorDataset(t_train_images, t_train_features, t_train_targets)
-
 train_dataloader = DataLoader(combined_tensor, batch_size=32, shuffle=True)
-
-
- # Dataloader
