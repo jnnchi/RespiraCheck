825fd5f4179010a2feb1cb0550db6abc7ee31c0b
Gabriel McFadyen <gabrielmcfadyen@gmail.com>
2025-01-08 14:50:43 -0500

    Split and normalized data, put into tensors. Image data is still not working/not split properly

--- data/data_loader.py
+++ data/data_loader.py
@@ -11,41 +11,68 @@ Classes:
 
 import pandas as pd
 from PIL import Image
+import numpy as np
 
-from torch.utils.data import Dataset, DataLoader
+import torch
 
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 import torchvision.transforms as transforms
 
-
+# Currently not transforming Images!!
 
 transform = transforms.Compose([
-    transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
-dataset = pd.read_csv('data/spectrograms/labels.csv')
+# Load Data
 
+dataset = pd.read_csv('data/spectrograms/numerical_labels.csv')
 
-# class ImageData(Dataset):
-#     def __init__(self, dataframe, transform=None):
-#         self.dataframe = dataframe
-#         self.transform = transform
+# Split Data
 
-#     def __len__(self):
-#         return len(self.dataframe)
+numerical_data = dataset.drop("filepath", axis=1)
+labels = dataset["diagnosis"]
+features = dataset.drop("diagnosis", axis=1)
+
+training_features, testing_features, training_target, testing_target = train_test_split(features, labels.to_frame(), test_size=0.15, shuffle=False)
+
+training_paths = training_features["filepath"]
+training_features.drop("filepath")
+
+testing_paths = testing_features["filepath"]
+testing_features.drop("filepath")
+
+print(training_features)
+# Make a np array of training images
+
+img_list = []
+for path in training_paths:
+    img = Image.open(path)
+    img_array = np.array(img)
+    img_list.append(img_array)
     
-#     def __getitem__(self, idx):
-#         img_path = self.dataframe.iloc[idx, 0]
-#         label = self.dataframe.iloc[idx, 1]
+image_array = np.array(img_list)
 
-#         image = Image.open(img_path).convert('RGB')
+# Normalize Data
 
-#         if self.transform:
-#             image = self.transform(image)
+scaler = MinMaxScaler()
+
+n_training_features = pd.DataFrame(scaler.fit_transform(training_features))
+n_testing_features = pd.DataFrame(scaler.fit_transform(testing_features))
+n_training_target = pd.DataFrame(scaler.fit_transform(training_target))
+n_testing_target = pd.DataFrame(scaler.fit_transform(testing_target))
+
+# Creating Tensors
+
+t_train_features = torch.Tensor(n_training_features.to_numpy())
+t_train_targets = torch.Tensor(n_training_target.to_numpy())
+t_train_images = torch.Tensor(image_array)
+
+combined_tensor = TensorDataset(t_train_images, t_train_features, t_train_targets)
+
+train_dataloader = DataLoader(combined_tensor, batch_size=32, shuffle=True)
 
 
-# dataset_object = ImageData(dataframe=dataset, transform=transform)
-
-# print(dataset_object[1])
-
- 
+ # Dataloader
