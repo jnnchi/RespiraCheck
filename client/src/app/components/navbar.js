67642be09575e5d0cd49dd50df3a957c45f4996e
aakshataa <121204544+aakshataa@users.noreply.github.com>
2025-02-16 00:55:58 -0500

    finalized navbar styling

--- client/src/app/components/navbar.js
+++ client/src/app/components/navbar.js
@@ -2,7 +2,7 @@
 
 import { Box, Typography } from "@mui/material";
 import React from "react";
-
+import Image from "next/image";
 
 const Navbar = () => {
   return (
@@ -15,50 +15,44 @@ const Navbar = () => {
         display: "flex",
         alignItems: "center",
         padding: "0 63px",
-        gap: 15,
-        justifyContent: "right",
+        
       }}
     >
-      <Box
-        component="img"
-        sx={{
-          width: "351px",
-          height: "73px",
-          objectFit: "cover",
-        }}
-        alt="Screenshot"
-        src={''}
-      />
 
-      <Typography
-        sx={{
-          fontSize: "25px",
-          color: "black",
-          lineHeight: "52.5px",
-        }}
-      >
-        Home
-      </Typography>
+      <Image src="/RespiraCheckLogo.png" width={200} height={80} alt="hi" sx={{justifyContent: 'left'}}></Image>
+        <Box sx={{ width: "100%", gap: 15, display: 'flex', justifyContent: "right"}}>
+          <Typography
+            sx={{
+              fontSize: "25px",
+              color: "black",
+              lineHeight: "52.5px",
+            }}
+          >
+            Home
+          </Typography>
 
-      <Typography
-        sx={{
-          fontSize: "25px",
-          color: "black",
-          lineHeight: "52.5px",
-        }}
-      >
-        Info
-      </Typography>
+          <Typography
+            sx={{
+              fontSize: "25px",
+              color: "black",
+              lineHeight: "52.5px",
+            }}
+          >
+            Info
+          </Typography>
 
-      <Typography
-        sx={{
-          fontSize: "25px",
-          color: "#3d70ec",
-          lineHeight: "52.5px",
-        }}
-      >
-        RespiraChecker
-      </Typography>
+          <Typography
+            sx={{
+              fontSize: "25px",
+              color: "#3d70ec",
+              lineHeight: "52.5px",
+            }}
+          >
+            RespiraChecker
+          </Typography>
+
+        </Box>
+      
     </Box>
   );
 };
