42579e013636666e3ec7dca08bfcb3ece39ac97e
aakshataa <121204544+aakshataa@users.noreply.github.com>
2025-02-23 10:21:03 -0500

    added use-the-tool page from current pr

--- client/src/app/pages/use-the-tool/page.js
+++ client/src/app/pages/use-the-tool/page.js
@@ -1,26 +1,23 @@
-import { Box, Typography, Stack } from "@mui/material";
-
-import UploadButton from '../../components/upload-button';
-import Steps from '../../components/steps';
-import Navbar from '../../components/navbar';
-
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../../theme/theme";
+import UploadAudio from "@/app/components/upload-audio";
+import RecordAudio from "@/app/components/record-audio";
+import SubmitAudioHeading from "@/app/components/submit-audio-heading";
 
 export default function Action() {
-    return (
-      <ThemeProvider theme={theme}>
-        <Navbar></Navbar>
-        {/* <Stack width= "100%" direction ="row" alignItems="center" sx={{ justifyContent: "center" }} >
-            <Box sx={{width: "30%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
-                <UploadButton></UploadButton>
-                <Typography>Upload Audio</Typography>
-            </Box>
-            <Steps></Steps>
+  return (
+      <ThemeProvider>
+      <Stack width= "100%" direction ="column" alignItems="center" spacing={10} sx={{ justifyContent: "center", mt: 6 }} >
+        <SubmitAudioHeading></SubmitAudioHeading>
 
-        </Stack> */}
-        
+        <Stack width= "100%" direction ="row" alignItems="center" spacing={22} sx={{ justifyContent: "center" }} >
+          <UploadAudio></UploadAudio>
+          <RecordAudio></RecordAudio>
+        </Stack>
 
-      </ThemeProvider>
-    );
-  }
+      </Stack>
+    </ThemeProvider>
+
+
+  );
+}
