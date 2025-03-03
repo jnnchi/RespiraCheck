3960ea4ee2db89d363a172892df9d294065c5998
Jennifer Chiou <jennifer.r.chiou@gmail.com>
2025-03-03 06:15:52 -0500

    fix client server error on home page

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,5 +1,7 @@
+"use client"; // This tells the server to send the client version of the page to the client
 import React from 'react';
 
+import { useEffect, useState } from "react";
 import Navbar from './components/navbar';
 import { Stack, Box } from '@mui/material';
 import Title from '@/app/components/home/title';
@@ -8,24 +10,32 @@ import InfoText from '@/app/components/home/info-text';
 import Image from 'next/image';
 import Link from 'next/link';
 
-
 const Home = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <div>
-      <Navbar></Navbar>
-      <Stack direction="row" spacing={0} sx={{paddingLeft: "120px", paddingTop: "50px"}}>
-        <Stack direction="column" spacing={5}>
-          <Title/>
-          <StartButton/>
-          <InfoText/>
-        </Stack>
-        <Box sx={{paddingRight: "120px", paddingTop: "50px", alignItems: "flex-start"}}>
-          <Image src="/undraw_medical_icon.svg" alt="medical image" width={500} height={367}/> 
-
-        </Box>
-      </Stack>
-
-      
+      {isClient ? (
+        <>
+          <Navbar />
+          <Stack direction="row" spacing={0} sx={{ paddingLeft: "120px", paddingTop: "50px" }}>
+            <Stack direction="column" spacing={5}>
+              <Title />
+              <StartButton />
+              <InfoText />
+            </Stack>
+            <Box sx={{ paddingRight: "120px", paddingTop: "50px", alignItems: "flex-start" }}>
+              <Image src="/undraw_medical_icon.svg" alt="medical image" width={500} height={367} />
+            </Box>
+          </Stack>
+        </>
+      ) : (
+        <p>Loading...</p> // Prevents mismatched server/client rendering
+      )}
     </div>
   );
 };
