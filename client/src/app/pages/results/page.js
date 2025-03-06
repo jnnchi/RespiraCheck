7b4beebfd657e1e8bd309d9cddc5893ddfa1d377
aakshataa <121204544+aakshataa@users.noreply.github.com>
2025-03-05 21:17:37 -0500

    added hyperlink on results page

--- client/src/app/pages/results/page.js
+++ client/src/app/pages/results/page.js
@@ -1,10 +1,11 @@
+"use client";
 import React from 'react';
 import Spectrogram from '@/app/components/results/spectrogram';
 import NextSteps from '@/app/components/results/next-steps';
 import TextHeader from '@/app/components/results/text-header';
 import Result from '@/app/components/results/result';
 import { Box, Stack, ThemeProvider, Typography } from "@mui/material";
-
+import Link from 'next/link';
 import Navbar from '../../components/navbar';
 
 const Results = () => {
@@ -18,18 +19,23 @@ const Results = () => {
                     <Stack direction="column" spacing={2}>
                         <TextHeader/>
                         <Result/>
-                        <Typography 
-                            sx={{
-                                position: "relative", 
-                                fontFamily: "'Spartan-Regular', Helvetica", 
-                                color: "#303030", 
-                                fontSize: "1.5rem",
-                                textDecoration: "underline",
-                                fontWeight: 200,
-                            }
-                        }>
-                        Learn more about our model &gt;
-                        </Typography>
+                        
+                       
+                            <Typography 
+                                sx={{
+                                    position: "relative", 
+                                    fontFamily: "'Spartan-Regular', Helvetica", 
+                                    color: "#303030", 
+                                    fontSize: "1.5rem",
+                                    textDecoration: "underline",
+                                    fontWeight: 200,
+                                }
+                            }>
+                                <Link href="/pages/about"> 
+                                    <span style={{ textDecoration: "none"}}>Learn more about our model&gt;</span>
+                                </Link>
+
+                            </Typography>
                     </Stack>
                 </Stack>
                 <Box sx={{ marginTop: "-50px !important"}}><NextSteps sx={{ marginTop: "0px !important"}}/></Box>
